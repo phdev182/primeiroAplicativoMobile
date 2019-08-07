0acf530c59b9f18dac31c9d2b3da8fb1301a1732
Pedro Henrique <se7code@gmail.com>
2019-08-07 19:56:00 -0400

    git

--- Backend/src/Controllers/DevController.js
+++ Backend/src/Controllers/DevController.js
@@ -1,9 +1,24 @@
 const axios = require('axios');
-
+const Dev = require('../models/dev');
 module.exports = {
   async store(req, res) {
-        const{ username } = req.body
+        const{ username } = req.body;
+        
+        const userExists = await Dev.findOne({ use: username })
+            if (userExists) {
+                return res.json(userExists);
+            }
+        
         const response = await axios.get(`https://api.github.com/users/${username}`)
-        return res.json(response.data);
+
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        })
+        return res.json(dev);
     }
 };
