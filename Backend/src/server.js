03817f7a4063b2f4432bea0de03d752d08a00896
Pedro Henrique <se7code@gmail.com>
2019-08-08 21:38:18 -0400

    arrumado

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,10 +5,10 @@ const cors = require('cors')
 
 const server = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-dnsux.mongodb.net/omnistack8?retryWrites=true&w=majority',
-{useNewUrlParser: true});
+    { useNewUrlParser: true });
 
 server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(3000);
+server.listen(3333);
