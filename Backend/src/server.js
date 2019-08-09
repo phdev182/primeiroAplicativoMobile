20f72e49923cff9379f2c96be37726a8bf978e4b
Pedro Henrique <se7code@gmail.com>
2019-08-08 20:30:00 -0400

    comit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors')
 
 const server = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-dnsux.mongodb.net/omnistack8?retryWrites=true&w=majority',
